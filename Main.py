d29b1bc29231ae161b2f061d70757d9b2d3f97bb
Samip Subedi <samip@geniusits.com.au>
2022-08-18 17:55:14 +0545

    Refactor of code remain

--- Main.py
+++ Main.py
@@ -1,39 +1,37 @@
 import json
-from unicodedata import name
 import requests
 from requests.exceptions import HTTPError
 import sqlite3
 
-class Main:
-    requests.get('https://fakestoreapi.com/products')
-    def make_get_request(url):
-        try:
-            response = requests.get(url)
-            # if the response was successful, no Exception will be raised
-            response.raise_for_status()
-        except HTTPError as http_err:
-            print(f'HTTP Error occured: {http_err}')
-        except Exception as err:
-            print(f'Other error occured: {err}')
-        else:
-            print('Successful get request', response)
-            return response
-    make_get_request('https://fakestoreapi.com/products')
-    response = make_get_request('https://fakestoreapi.com/products')
-    t = response.json()
-    #new_dict = {item['id']:item for item in t}
-    new_dict = t 
 
-    for item in new_dict:
-        item['rate'] = item['rating']['rate']
-        item['count']= item['rating']['count']
-    #print(new_dict)
-    
-    #Creating Table
-    conn = sqlite3.connect('store.db')
-    c = conn.cursor()
-    c.executescript(
-        """DROP TABLE IF EXISTS Product;
+def make_get_request(url):
+    try:
+        response = requests.get(url)
+        # if the response was successful, no Exception will be raised
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP Error occured: {http_err}')
+    except Exception as err:
+        print(f'Other error occured: {err}')
+    else:
+        print('Successful get request', response)
+        return response
+
+
+make_get_request('https://fakestoreapi.com/products')
+response = make_get_request('https://fakestoreapi.com/products')
+t = response.json()
+
+new_dict = t
+for item in new_dict:
+    item['rate'] = item['rating']['rate']
+    item['count'] = item['rating']['count']
+# Creating Table
+conn = sqlite3.connect('store.db')
+conn.row_factory = sqlite3.Row
+c = conn.cursor()
+c.executescript(
+    """DROP TABLE IF EXISTS Product;
             CREATE TABLE Product (
             id INT,
             title TEXT,
@@ -45,34 +43,35 @@ class Main:
             count INT
             );
         """)
-    
-    #ADDING VALUE TO TABLE   
-    product_values = new_dict  
-    c.executemany("INSERT INTO Product VALUES(:id,:title,:price,:description,:category,:image,:rate,:count)", product_values)
-    conn.commit()
-    c.execute("SELECT * from Product;").fetchall()
-    c.execute("SELECT * from Product;")
-    
-    print("WELCOME TO GROCERY SHOP")
-    name = input("Enter your name: ")
-    print("___________________________________________________________________________________________")
-    print("\n Hello " +name+ " HERE IS THE ITEM WE HAVE AVAILABLE ON THE SHOP")
-    print("\n ___________________________________________________________________________________________")
-    for row in c.fetchall():
-        t_id, t_title, t_price, t_description, t_category, t_image, t_rate, t_count = row 
-        print(f" id : {t_id} || title:  {t_title } || price: {t_price} || \n desc:  {t_description} \
-            || \n category: {t_category} || \n imagelink: {t_image} || \n rate: {t_rate} || quantity: {t_count}")
-    
-    pid = int(input("Enter product id: "))
-    quantity = int(input("Enter product quantity you want to buy"))
-    
-       
-    
 
-   
+# ADDING VALUE TO TABLE
+product_values = new_dict
+c.executemany(
+    "INSERT INTO Product VALUES(:id,:title,:price,:description,:category,:image,:rate,:count)", product_values)
+conn.commit()
 
+c.execute("SELECT * from Product;")
+values = c.fetchall()
+items = [{k: item[k] for k in item.keys()}for item in values]
 
+print("WELCOME TO GROCERY SHOP")
+name = input("Enter your name: ")
+print("___________________________________________________________________________________________")
+print("\n Hello " + name + " HERE IS THE ITEM WE HAVE AVAILABLE ON THE SHOP")
+print(" ___________________________________________________________________________________________")
 
+for i in items:
+    print(i["id"], i["title"], i["price"])
 
+a = 5
 
-        
+productslist = int(input('Enter the Id of Product you want to buy: '))
+quantity = int(input("Enter the quantity of the product you want to buy: "))
+c = next((i for i in items if i['id'] == productslist))
+total = c['price'] * quantity
+print(f"Your Total price is : {str(total)}")
+
+trial = c['id'], c['title'],  total, quantity
+
+with open('my_file_2.txt', 'w') as f:
+    f.write(str(trial))
